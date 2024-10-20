da31111c8549901f780677f2a6d987335c44b25a
Anik Hasan <arfinhasananik@gmail.com>
2024-10-20 19:13:03 +0600

    first commit

---
+++ lib/src/model/product_size_type.dart
@@ -0,0 +1,10 @@
+
+import 'categorical.dart';
+import 'numerical.dart';
+
+class ProductSizeType {
+  List<Numerical>? numerical;
+  List<Categorical>? categorical;
+
+  ProductSizeType({this.numerical, this.categorical});
+}
